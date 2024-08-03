bf7b8ad391fb3bf4794b19d536a316dab169aedf
xiao_o <129086532+xccvde@users.noreply.github.com>
2024-08-03 16:41:33 +0800

    Add files via upload

---
+++ glmall-order/src/test/java/org/example/glmall/order/GlmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.glmall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
