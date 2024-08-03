48f46d7d155f488ad23c48d07aa9a7ae7bf9d0e2
xiao_o <129086532+xccvde@users.noreply.github.com>
2024-08-03 15:41:13 +0800

    Initial commit

---
+++ glmall-order/src/main/java/org/example/glmall/order/GlmallOrderApplication.java
@@ -0,0 +1,13 @@
+package org.example.glmall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlmallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlmallOrderApplication.class, args);
+    }
+
+}
