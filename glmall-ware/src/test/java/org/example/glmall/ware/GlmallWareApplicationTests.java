7aff37f7f54dc655b3ba7d8659ac57cd390995b4
xiao_o <129086532+xccvde@users.noreply.github.com>
2024-08-03 16:40:25 +0800

    Add files via upload

---
+++ glmall-ware/src/test/java/org/example/glmall/ware/GlmallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.glmall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
